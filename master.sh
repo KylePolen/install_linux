b6ec2ffcafacf12a578c01833f3cb89bab8175f5
KylePolen <67399556+KylePolen@users.noreply.github.com>
2023-01-03 11:19:57 -0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,35 +3,15 @@ scriptname=master.sh
 clear
 
 source ~/install/functions/global.sh
-source ~/install/functions/fixes.sh
-
-echo "sourced"
-read -p ""
 
 ~/install/functions/branding.sh
-echo "branding"
-read -p ""
 ~/install/scripts/intel_display.sh
-echo "intdis"
-read -p ""
 ~/install/scripts/highpoint.sh
-echo "hp"
-read -p ""
 ~/install/functions/drivers.sh
-echo "drivers"
-read -p ""
 ~/install/functions/software.sh
-echo "software"
-read -p ""
 ~/install/functions/updates.sh
-echo "updates"
-read -p ""
 ~/install/scripts/mount.sh
-echo "mount"
-read -p ""
 ~/install/functions/fixes.sh
-echo "fixes"
-read -p ""
 
 #Stop autorun
 _os_check
