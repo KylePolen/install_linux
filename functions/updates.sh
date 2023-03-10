937321e2eaca7bce7e556699c95028923f969dd5
KylePolen <67399556+KylePolen@users.noreply.github.com>
2023-03-10 13:51:44 -0800

    progress

--- functions/updates.sh
+++ functions/updates.sh
@@ -9,7 +9,7 @@ _update_desktop() {
 		sudo apt update
 		sudo DEBIAN_FRONTEND=nointeractive apt install ubuntu-restricted-extras -y
 		sudo DEBIAN_FRONTEND=nointeractive apt dist-upgrade -y
-		sudo DEBIAN_FRONTEND=nointeractive apt install mdadm net-tools finger xfsprogs dkms build-essential apt-utils ssh snap shfmt -y
+		sudo DEBIAN_FRONTEND=nointeractive apt install mdadm net-tools pv finger xfsprogs dkms build-essential apt-utils ssh snap shfmt -y
 		sudo DEBIAN_FRONTEND=nointeractive apt install gparted bolt gedit gdebi smartmontools smbclient p7zip-full mesa-utils git -y
 		if [ $osversion == "20.04" ]; then
 			sudo DEBIAN_FRONTEND=nointeractive apt install libxml2:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386 libatk-adaptor:i386 -y
@@ -26,7 +26,7 @@ _update_server() {
 		sudo DEBIAN_FRONTEND=nointeractive apt --fix-broken install -y
 		sudo apt update
 		sudo DEBIAN_FRONTEND=nointeractive apt dist-upgrade -y
-		sudo DEBIAN_FRONTEND=nointeractive apt install build-essential bolt finger smartmontools p7zip-full smbclient network-manager ssh dkms samba cups mdadm net-tools -y
+		sudo DEBIAN_FRONTEND=nointeractive apt install build-essential bolt finger pv smartmontools p7zip-full smbclient network-manager ssh dkms samba cups mdadm net-tools -y
 		if [ $osversion == "20.04" ]; then
 			sudo DEBIAN_FRONTEND=nointeractive apt dist-upgrade linux-generic-hwe-20.04 -y
 		fi
