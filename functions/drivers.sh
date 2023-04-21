043d6b7bc377cd94e474ffa0852445ad3503a669
KylePolen <67399556+KylePolen@users.noreply.github.com>
2023-04-21 12:42:45 -0700

    Update drivers.sh

--- functions/drivers.sh
+++ functions/drivers.sh
@@ -8,9 +8,9 @@ _nvidia() {
 		sudo DEBIAN_FRONTEND=nointeractive add-apt-repository -y ppa:graphics-drivers/ppa
 		sudo apt update
 		if [ "$ostype" == "Desktop" ]; then
-			sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-525
+			sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-530
 		else
-			sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-525 --no-install-recommends
+			sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-530 --no-install-recommends
 		fi	
 	fi
 }
@@ -33,34 +33,36 @@ _aspeed() {
 #Highpoint 7505
 _hp7505() {
 if lspci -v | grep -q '7505\|SSD7505'; then
-	if [ ! -f /etc/init.d/hptdrv-monitor ]; then
-		clear
-		echo 'Highpoint 7505 detected. The system will reboot after installing drivers.'
-		echo 'The script will automatically restart once you log back in.'
-		echo 'Press any key to continue.'
-		read -p ""
-		cd ~/install/assets
-		wget -c https://github.com/KylePolen/install_linux/raw/main/assets/drivers/hp7505/hptnvme_g5_linux_src_v1.4.1_2022_03_04.bin
-		wget -c https://github.com/KylePolen/install_linux/raw/main/assets/drivers/hp7505/RAID_Manage_Linux_v3.1.5_22_06_10.bin
-		sudo chmod +x *.bin
-		sudo ./hptnvme_g5_linux_src_v1.4.1_2022_03_04.bin
-		sudo ./RAID_Manage_Linux_v3.1.5_22_06_10.bin
-		sleep 5
-		if [ "$motherboard" == "X299 AORUS Gaming 7" ]; then
-			sudo sed -i 's/ iommu=off//' /etc/default/grub
-			sudo update-grub
+	if grep -q 'RAID Setup (Add-In Controller)' ~/install/orderdata/orderdata; then
+		if [ ! -f /etc/init.d/hptdrv-monitor ]; then
+			clear
+			echo 'Highpoint 7505 detected. The system will reboot after installing drivers.'
+			echo 'The script will automatically restart once you log back in.'
+			echo 'Press any key to continue.'
+			read -p ""
+			cd ~/install/assets
+			wget -c https://github.com/KylePolen/install_linux/raw/main/assets/drivers/hp7505/hptnvme_g5_linux_src_v1.4.1_2022_03_04.bin
+			wget -c https://github.com/KylePolen/install_linux/raw/main/assets/drivers/hp7505/RAID_Manage_Linux_v3.1.5_22_06_10.bin
+			sudo chmod +x *.bin
+			sudo ./hptnvme_g5_linux_src_v1.4.1_2022_03_04.bin
+			sudo ./RAID_Manage_Linux_v3.1.5_22_06_10.bin
+			sleep 5
+			if [ "$motherboard" == "X299 AORUS Gaming 7" ]; then
+				sudo sed -i 's/ iommu=off//' /etc/default/grub
+				sudo update-grub
+			fi
+			if [ $ostype == "Server" ]; then
+				sudo sed -i '/~\/install/d' ~/.bashrc
+				echo '~/install/master.sh' >>~/.bashrc
+			else
+				sudo rm /etc/profile/.d/master.sh
+				echo "sleep 5" | sudo tee -a /etc/profile.d/master.sh
+				echo 'gnome-terminal -- ~/install/master.sh' | sudo tee -a /etc/profile.d/master.sh
+			fi
+			sudo reboot
+			exit
 		fi
-		if [ $ostype == "Server" ]; then
-			sudo sed -i '/~\/install/d' ~/.bashrc
-			echo '~/install/master.sh' >>~/.bashrc
-		else
-			sudo rm /etc/profile/.d/master.sh
-			echo "sleep 5" | sudo tee -a /etc/profile.d/master.sh
-			echo 'gnome-terminal -- ~/install/master.sh' | sudo tee -a /etc/profile.d/master.sh
-		fi
-		sudo reboot
-		exit
-	fi
+	fi	
 fi
 }
 
