b1c467f00a1a61a8ae96d97ac7ba5918a4062794
KylePolen <kyle@pugetsystems.com>
2024-02-23 13:27:17 -0800

    Update drivers.sh

--- functions/drivers.sh
+++ functions/drivers.sh
@@ -13,8 +13,8 @@ _nvidia() {
 		sudo apt update
 		if [ "$ostype" == "Desktop" ]; then
 			sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-$revision
-		else
-			sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-525 --no-install-recommends
+		#else
+			#sudo DEBIAN_FRONTEND=nointeractive apt -y install nvidia-driver-$revision --no-install-recommends
 		fi	
 	fi
 }
