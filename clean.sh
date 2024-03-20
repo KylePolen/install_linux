b55432cb5ad6fb3d8ceaa2f4b167317dd3e25939
KylePolen <kyle@pugetsystems.com>
2024-03-20 16:17:31 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -26,3 +26,14 @@ history -cw
 >~/.bash_history
 rm ~/.bash_history >/dev/null 2>&1
 history -cw
+
+###Create password readme on desktop
+echo "These are the credentials specified by Puget Systems during OS installation."
+echo.
+echo "username: user"
+echo "password: Password1"
+echo.
+echo.
+echo "It's highly recommended that you update the password upon receiving the system."
+echo 'To do so, open a terminal and type "sudo passwd user" then enter "Password1" as the password.'
+echo "Now enter your new password and you're done."
