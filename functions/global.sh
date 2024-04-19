7d29dd6df23144b70c90dcd5212100ec8211ffe6
KylePolen <kyle@pugetsystems.com>
2024-04-19 13:12:16 -0700

    readme

--- functions/global.sh
+++ functions/global.sh
@@ -123,10 +123,5 @@ _branding() {
 		echo "Puget Systems Default Credentials" | tee -a /home/$USER/Puget_Systems_Readme
 		echo "Username: user" | tee -a /home/$USER/Puget_Systems_Readme
 		echo "Password: Password1!" | tee -a /home/$USER/Puget_Systems_Readme
-		echo "" | tee -a /home/$USER/Puget_Systems_Readme
-		echo "==================================================" | tee -a /home/$USER/Puget_Systems_Readme
-		echo "" | tee -a /home/$USER/Puget_Systems_Readme
-		echo "By default Puget Systems does not set a root password. To change a password run 'sudo passwd <user>', where <user> is the user in question i.e. 'root', enter the current password and then set the new password" | tee -a /home/$USER/Puget_Systems_Readme
-		echo "Example: 'sudo passwd root'" | tee -a /home/$USER/Puget_Systems_Readme
 	fi
 }
