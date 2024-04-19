031f5e750cd161d5979bafecc1c1ec59af081d26
KylePolen <kyle@pugetsystems.com>
2024-04-19 13:07:04 -0700

    readme

--- test.sh
+++ test.sh
@@ -2,13 +2,11 @@
 scriptname="test.sh"
 clear
 
-		echo "Puget Systems Default Credentials" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "Username: user" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "Password: Password1!" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "==================================================" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "By default Puget Systems does not set a root password" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
-		echo "To change a password run 'sudo passwd <user>', where <user> is the user in question i.e. 'root', enter the current sudo password and then set the new password" | sudo tee -a /home/$USER/Puget\ Systems\ Readme
+echo "Puget Systems Default Credentials" | tee -a /home/$USER/Puget_Systems_Readme
+echo "Username: user" | tee -a /home/$USER/Puget_Systems_Readme
+echo "Password: Password1!" | tee -a /home/$USER/Puget_Systems_Readme
+echo "" | tee -a /home/$USER/Puget_Systems_Readme
+echo "==================================================" | tee -a /home/$USER/Puget_Systems_Readme
+echo "" | tee -a /home/$USER/Puget_Systems_Readme
+echo "By default Puget Systems does not set a root password. To change a password run 'sudo passwd <user>', where <user> is the user in question i.e. 'root', enter the current password and then set the new password" | tee -a /home/$USER/Puget_Systems_Readme
+echo "Example: 'sudo passwd root'" | tee -a /home/$USER/Puget_Systems_Readme
