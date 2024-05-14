d2cb1ba0d6a33ff38119dc07919c66ecc0c7109c
KylePolen <kyle@pugetsystems.com>
2024-05-14 09:28:12 -0700

    Update drivers.sh

--- functions/drivers.sh
+++ functions/drivers.sh
@@ -158,7 +158,7 @@ if lspci -v | grep '9560'; then
 	sudo chown -R $USER /opt/lsi
 	sudo ln -sf /usr/local/lib/libslp.so.1.0.0 /opt/lsi/LSIStorageAuthority/bin/libslp.so.1
 	cd ~/install/assets/drivers/broadcom9560RAID/LSA/WebGUIRelease-DCSG01688241/webgui_rel/gcc_11.2.x
-	./install_deb.sh
+	./install_deb.sh -4 -2
 	cd ~/install
 fi
 }
